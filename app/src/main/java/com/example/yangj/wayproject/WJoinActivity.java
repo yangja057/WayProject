4adc5cb3880af4045dba9640d73cf3634c22d6a3
tlaektmf <dazzel0826@gmail.com>
2017-11-19 19:19:00 +0900

    수정

--- app/src/main/java/com/example/yangj/wayproject/WJoinActivity.java
+++ app/src/main/java/com/example/yangj/wayproject/WJoinActivity.java
@@ -19,16 +19,60 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class WJoinActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
-
+    //변수선언
+    private FirebaseAuth mAuth;
+    private EditText editText_Email;
+    private EditText editText_Password;
+    private Button button_EmailLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wjoin);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
+        editText_Email=(EditText)findViewById(R.id.idEmailEdit);
+        editText_Password=(EditText)findViewById(R.id.idPasswordEdit);
+        button_EmailLogin=(Button)findViewById(R.id.idEmailLoginBtn);
+        button_EmailLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(WJoinActivity.this,"버튼은 눌림",Toast.LENGTH_SHORT).show();
+                createUser(editText_Email.getText().toString(),editText_Password.getText().toString());
+            }
+        });
 
     }
 
+    /**
+     * 회원을 비밀번호와 이메일로 등록해주는 함수
+     */
+    private void createUser(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "createUserWithEmail:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+//                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+//                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                            Toast.makeText(WJoinActivity.this,"회원가입성공",Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
 
 
 

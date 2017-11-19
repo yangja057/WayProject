550dc5b42514d4c91838b00091fc7341cef1ae27
tlaektmf <dazzel0826@gmail.com>
2017-11-19 23:50:31 +0900

    버튼에 클래스 삽입해주세용

--- app/src/main/java/com/example/yangj/wayproject/MainActivity.java
+++ app/src/main/java/com/example/yangj/wayproject/MainActivity.java
@@ -5,16 +5,78 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnGoToJoin;
+    private Button btnGoToApi;
+    private Button btnGoToMainView;
+    private Button btnGoRegiReview;
+    private Button btnGoToLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      Intent intent=new Intent(this,WJoinActivity.class);
-       startActivity(intent);
+        /*
+        로그인버튼 : 로그인으로 이동
+        회원가입 버튼 : 회원가입으로 이동
+        검색버튼 : 구글 api로 이동
+        보여지는 view: 출발 도착을 눌렀을 경우 그다음에 보여지는 썸네일들이 있는 뷰
+        리뷰등록 버튼 : 리뷰등록 액티비티로 이동
+
+         */
+
+        btnGoToJoin=(Button)findViewById(R.id.goToJoin);
+        btnGoToApi=(Button)findViewById(R.id.goToApi);
+        btnGoToMainView=(Button)findViewById(R.id.goToMainView);
+        btnGoRegiReview=(Button)findViewById(R.id.goRegiReview);
+        btnGoToLogin=(Button)findViewById(R.id.goToLogin);
+
+
+        btnGoToJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getBaseContext(),WJoinActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//        btnGoToApi.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent=new Intent(getBaseContext(),//클래스 등록);
+//                startActivity(intent);
+//            }
+//        });
+
+//        btnGoToMainView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent=new Intent(getBaseContext(),//클래스 등록);
+//                startActivity(intent);
+//            }
+//        });
+
+//        btnGoRegiReview.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent=new Intent(getBaseContext(),//클래스 등록);
+//                startActivity(intent);
+//            }
+//        });
+
+        btnGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getBaseContext(),WLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
 
     }
 }
--- app/src/main/java/com/example/yangj/wayproject/WLoginActivity.java
+++ app/src/main/java/com/example/yangj/wayproject/WLoginActivity.java
@@ -1,37 +1,13 @@
 package com.example.yangj.wayproject;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
-public class WLoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
-
+public class WLoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wlogin2);
-        Intent intent=new Intent(this, WRegiReviewActivity.class);
-        startActivity(intent);
-
-
-    }//oncreate closed
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        setContentView(R.layout.activity_wlogin);
     }
 }

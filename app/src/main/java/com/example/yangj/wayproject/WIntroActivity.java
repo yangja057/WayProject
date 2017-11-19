c8f2f5baad1aec0693217e20ce3290c557896496
Yeori <yoeri1123@naver.com>
2017-11-19 19:07:37 +0900

    요리인트로화면

---
+++ app/src/main/java/com/example/yangj/wayproject/WIntroActivity.java
@@ -0,0 +1,31 @@
+package com.example.yangj.wayproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by USER on 2017-11-19.
+ */
+
+public class WIntroActivity extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_wintro);
+        startLoading();
+    }
+    private void startLoading(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}

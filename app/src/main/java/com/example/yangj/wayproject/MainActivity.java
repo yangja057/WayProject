cbdd7188ddd8a7eb006cbd29e83e66403976d252
tlaektmf <dazzel0826@gmail.com>
2017-11-19 20:02:36 +0900

    확인 제발

--- app/src/main/java/com/example/yangj/wayproject/MainActivity.java
+++ app/src/main/java/com/example/yangj/wayproject/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      Intent intent=new Intent(this,WLoginActivity.class);
+      Intent intent=new Intent(this,WJoinActivity.class);
        startActivity(intent);
 
     }

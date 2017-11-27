a863193909b910b49c145542404e89c467eeae65
Yeori <yoeri1123@naver.com>
2017-11-27 22:53:34 +0900

    여기까지입니댜 ㅎㅅㅎ 집가서하겠습니니다.ㅎㅅㅎ

--- app/src/main/java/com/example/yangj/wayproject/WRegiReviewAdapter.java
+++ app/src/main/java/com/example/yangj/wayproject/WRegiReviewAdapter.java
@@ -3,6 +3,8 @@ package com.example.yangj.wayproject;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -58,7 +60,7 @@ public class WRegiReviewAdapter extends BaseAdapter{
             convertView=inflater.inflate(R.layout.activity_wregi_review_item, parent, false);
         }
         regiReviewItem.placeButton=(Button) convertView.findViewById(R.id.placeButton);
-        UserImage=(ImageButton) convertView.findViewById(R.id.UserImage);
+        regiReviewItem.UserImage=(ImageButton) convertView.findViewById(R.id.UserImage);
         EditText explainText=(EditText)convertView.findViewById(R.id.explainText);
 
         regiReviewItem.placeButton.setOnClickListener(new View.OnClickListener() {
@@ -68,16 +70,17 @@ public class WRegiReviewAdapter extends BaseAdapter{
             }
         });
 
-       UserImage.setImageResource(regiReviewItem.getPhoto());
+       regiReviewItem.UserImage.setImageResource(regiReviewItem.getPhoto());
 
-       UserImage.setOnClickListener(new View.OnClickListener() {
+      regiReviewItem.UserImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //디바이스에서 가져오기
+                showFileChooser();
             }
         });
 
-       UserImage.setOnLongClickListener(new View.OnLongClickListener(){
+      regiReviewItem.UserImage.setOnLongClickListener(new View.OnLongClickListener(){
             @Override
             public boolean onLongClick(View v) {
                 Toast.makeText(context, "길게눌려여",Toast.LENGTH_SHORT).show();
@@ -126,4 +129,12 @@ public class WRegiReviewAdapter extends BaseAdapter{
         WRegiReviewItemList.add(item);
 
     }
+
+    private void showFileChooser(){
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+      //  startActivityForResult(Intent.createChooser(intent,"Select an Image"),PICK_IMAGE_REQUEST);
+    }
+
 }

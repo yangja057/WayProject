cf53e36f96e131ac3be4070ad7a59416f589aae7
tlaektmf <dazzel0826@gmail.com>
2017-12-14 16:52:40 +0900

    listActivity item한개만

--- app/src/main/java/com/example/yangj/wayproject/WListActivity.java
+++ app/src/main/java/com/example/yangj/wayproject/WListActivity.java
@@ -213,18 +213,21 @@ public class WListActivity extends AppCompatActivity {
                         //데이터가 날라온 것을 이미지 리스트에 담는다
                         listItems.clear();//수정될때마다 데이터가 날라옴/ 안해주면 데이터가 쌓여
 
-                        for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    /*
-                    DataSnapshot snapshot : dataSnapshot.getChildren() //처음부터 끝까지 데이터를 읽는다는 뜻임
-                    getchildren() :가지 하나를 children이라고 함
-                    child==현재 "review" 의 children하나를 읽어옴
-                     */
-                            ImageData imageData=snapshot.getValue(ImageData.class);
-                            //listItems.add(imageData);//데이터의 개수만큼 for loop을 돌면서 list에 객체를 넣음
-                            adapter.WImageDataItemList.add(imageData);
-                            Log.i("다스리의 로그", "imageData:" + imageData.getUserEmail());
-                        }
+//                        for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+//                    /*
+//                    DataSnapshot snapshot : dataSnapshot.getChildren() //처음부터 끝까지 데이터를 읽는다는 뜻임
+//                    getchildren() :가지 하나를 children이라고 함
+//                    child==현재 "review" 의 children하나를 읽어옴
+//                     */
+//
+//                            ImageData imageData=snapshot.getValue(ImageData.class);
+//                            //listItems.add(imageData);//데이터의 개수만큼 for loop을 돌면서 list에 객체를 넣음
+//                            //adapter.WImageDataItemList.add(imageData);
+//                            Log.i("다스리의 로그", "imageData:" + imageData.getUserEmail());
+//                        }
 
+                        ImageData imageData=dataSnapshot.getValue(ImageData.class);
+                        adapter.WImageDataItemList.add(imageData);
                         adapter.notifyDataSetChanged();//새로 고침(갱신되니까)
                     }
 
